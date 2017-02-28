12b7a920ba372d609689e5790f7cdcfd3d4fcad9
jaumealonso21 <jaumealonso@hotmail.com>
2017-02-28 19:34:09 +0100

    Versió 0.0

---
+++ public_html/formi.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ 
+//$( "button" ).click(function() {
+//  $( "td" ).each(function( index, element ) {
+//    // element == this
+//    if ( $( this ).html() === "" ) {
+//        $( element ).css( "backgroundColor", "yellow" );
+//    }
+//    
+//  });
+//});
+
+
+});
+
