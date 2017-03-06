9d7370e866737bb4fcf39e1ff7fc4d8cbfe6e1eb
jaumealonso21 <jaumealonso@hotmail.com>
2017-03-06 17:24:37 +0100

    Versió 0.5

--- public_html/formi.js
+++ public_html/formi.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
  
-//$( "button" ).click(function() {
-//  $( "td" ).each(function( index, element ) {
-//    // element == this
-//    if ( $( this ).html() === "" ) {
-//        $( element ).css( "backgroundColor", "yellow" );
-//    }
-//    
-//  });
-//});
+$( "#textarea01" ).keydown(function(e) {
+    var limit   = $(this).attr("maxlength","100");
+    var valor  = $(this).val();
+    var cars = limit.attr("maxlength") - valor.length;
+    if (e.keyCode === 8) {
+        cars = cars - 1;     
+    }
+    $("#text01").html("Et falten: "+ cars +" caràcters");
+});
 
+$( "#textarea02" ).keydown(function(e) {
+//    if (e.keyCode === 87 && e.keyCode === 69 && e.keyCode === 82) {
+    if (e.keyCode === 87) {
+        var cont = $(this).val();
+        cont = cont + e;
+        $("#textarea02").html(e.keyCode);
+    }else{
+        $("#textarea02").html(" ");
+    }
+//    $("#textarea02").html(" ");
+});
 
 });
 
