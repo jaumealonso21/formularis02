ab60969a252fde5972c3a6c08b4d0e19e2656d79
jaumealonso21 <jaumealonso@hotmail.com>
2017-03-06 17:33:24 +0100

    Versió 0.6 funcions DNI i telf

--- public_html/formi.js
+++ public_html/formi.js
@@ -22,5 +22,34 @@ $( "#textarea02" ).keydown(function(e) {
 //    $("#textarea02").html(" ");
 });
 
+function nif_valido($nif) {
+    $nif = strtoupper($nif);
+     
+    $nifRegEx = '/^[0-9]{8}[A-Z]$/i';
+    $nieRegEx = '/^[XYZ][0-9]{7}[A-Z]$/i';
+     
+    $letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+     
+    if (preg_match($nifRegEx, $nif)) return ($letras[(substr($nif, 0, 8) % 23)] === $nif[8]);
+    else if (preg_match($nieRegEx, $nif)) {
+        if ($nif[0] === "X") $nif[0] = "0";
+        else if ($nif[0] === "Y") $nif[0] = "1";
+        else if ($nif[0] === "Z") $nif[0] = "2";
+ 
+        return ($letras[(substr($nif, 0, 8) % 23)] === $nif[8]);
+    }
+    else return false;
+}
+
+/*****************************/
+/*       Teléfono fijo       */
+/*****************************/
+ 
+function fijo_valido($n) {
+    $re = '/^(8|9)[0-9]{8}$/';
+     
+    return (preg_match($re, $n));
+}
+
 });
 
