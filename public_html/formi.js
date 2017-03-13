1222ff3e7fbf374674d1ea4317c3b4fae67d76d7
jaumealonso21 <jaumealonso@hotmail.com>
2017-03-13 12:07:08 +0100

    Version afegida

--- public_html/formi.js
+++ public_html/formi.js
@@ -9,6 +9,15 @@ $( "#textarea01" ).keydown(function(e) {
     }
     $("#text01").html("Et falten: "+ cars +" caràcters");
 });
+$( "#textarea01" ).change(function(e) {
+    var limit   = $(this).attr("maxlength","100");
+    var valor  = $(this).val();
+    var cars = limit.attr("maxlength") - valor.length;
+    if (e.keyCode === 8) {
+        cars = cars - 1;     
+    }
+    $("#text01").html("Et falten: "+ cars +" caràcters");
+});
 
 $( "#textarea02" ).keydown(function(e) {
 //    if (e.keyCode === 87 && e.keyCode === 69 && e.keyCode === 82) {
